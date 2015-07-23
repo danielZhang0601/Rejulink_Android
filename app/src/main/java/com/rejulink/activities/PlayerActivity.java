6396f5084cf3909788a9da2d75c64a85f8622d24
danielZhang0601 <daniel.zhang@wolaidai.com>
2015-07-23 18:57:11 +0800

    add player activity -- add fullscreen

--- app/src/main/java/com/rejulink/activities/PlayerActivity.java
+++ app/src/main/java/com/rejulink/activities/PlayerActivity.java
@@ -2,6 +2,8 @@ package com.rejulink.activities;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
 
 import com.rejulink.R;
 import com.rejulink.utils.L;
@@ -13,13 +15,16 @@ import com.rejulink.widgets.ZSurfaceView;
 public class PlayerActivity extends BaseActivity {
 
     private ZSurfaceView zsurface_view_video;
+    private DisplayMetrics displayMetrics;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
         L.e("onCreate");
-        zsurface_view_video = (ZSurfaceView)findViewById(R.id.zsurface_view_video);
+        zsurface_view_video = (ZSurfaceView) findViewById(R.id.zsurface_view_video);
+        displayMetrics = getResources().getDisplayMetrics();
+        zsurface_view_video.resizeSurfaceView(false, displayMetrics.widthPixels, displayMetrics.heightPixels);
     }
 
     @Override
@@ -37,6 +42,23 @@ public class PlayerActivity extends BaseActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         L.e("onConfigurationChanged" + newConfig.toString());
-        zsurface_view_video.resizeSurfaceView(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE);
+        zsurface_view_video.resizeSurfaceView(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        setDisplayType(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE);
     }
+
+    private void setDisplayType(boolean isFullScreen) {
+        int uiOpts;
+        if (isFullScreen) {
+            uiOpts = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE;
+        } else {
+            uiOpts = View.SYSTEM_UI_FLAG_VISIBLE;
+        }
+        getWindow().getDecorView().setSystemUiVisibility(uiOpts);
+    }
+
 }
